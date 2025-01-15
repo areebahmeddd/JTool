2cab53734e33a90d7e61b99976683e099925de4f
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2025-01-15 23:51:36 +0530

    feat: serialize dict type

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 def serialize(obj):
+    # String
     if isinstance(obj, str):
         return f'"{obj}"'
 
-    elif isinstance(obj, int):
+    # Integer or Float
+    elif isinstance(obj, (int, float)):
         return str(obj)
 
+    # Boolean
     elif isinstance(obj, bool):
         return 'true' if obj else 'false'
 
+    # None se null karna h
     elif obj is None:
         return 'null'
+
+    # Dictionary
+    elif isinstance(obj, dict):
+        items = []
+        for key, value in obj.items():
+            serialized_key = serialize(key)
+            serialized_value = serialize(value)
+            items.append(f'{serialized_key}:{serialized_value}')
+            return '{' + ','.join(items) + '}'
