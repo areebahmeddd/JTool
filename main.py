816ac8b3067da001ba45ed081b249c771c579501
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2025-01-16 00:24:34 +0530

    test: test code

---
+++ main.py
@@ -0,0 +1,17 @@
+from serialize import serialize
+from deserialize import deserialize
+
+if __name__ == '__main__':
+    # Test 1: Serialization
+    data = {'name': 'Areeb', 'age': 21, 'is_student': True}
+    serialized_data = serialize(data)
+    print(f'Serialized Data: {serialized_data}')
+
+    # Test 2: Deserialization
+    json_str = '{"name": "Areeb", "age": 21, "is_student": true}'
+    deserialized_data = deserialize(json_str)
+    print(f'\nDeserialized Data: {deserialized_data}')
+
+    # Test 3: Serialization and then Deserialization
+    deserialized_back = deserialize(serialized_data)
+    print(f'\nDeserialized Back from Serialized Data: {deserialized_back}')
